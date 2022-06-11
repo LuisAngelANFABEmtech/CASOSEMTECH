2f3f2e68c9c0288916ec560b9af97cd6d8294b37
LuisAngelANFABEmtech <107011128+LuisAngelANFABEmtech@users.noreply.github.com>
2022-06-11 17:27:40 -0600

    Ejercicio #1
    primera versión del caso retador 1

---
+++ main.py
@@ -0,0 +1,20 @@
+PoblaciónSinaloa_Hombres= int(1494815)
+PoblaciónSinaloa_Mujeres= int(1532125)
+
+Población_TOTAL=(PoblaciónSinaloa_Hombres+PoblaciónSinaloa_Mujeres)
+
+print("SINALOA TIENE UNA POBLACIÓN TOTAL DE:",(Población_TOTAL),"habitantes")
+
+porcentaje_culiacan=float(33.15)
+porcentaje_mazatlan=float(16.57)
+
+Porcentaje_total=(porcentaje_culiacan+porcentaje_mazatlan)
+
+print("Sus principales ciudades son: Culiacan y Mazatlan y juntas albergan un total de:",(Porcentaje_total),"% de habitantes")
+
+Temperaturamedia_Gcentigrados=float(25.45)
+
+Tipos_de_climas=["Cálido","Subhúmedo","seco","semiseco"]
+
+print("Su temperatura media es de",(Temperaturamedia_Gcentigrados),"grados centígrados")
+print("sus tipos de clima son:",(Tipos_de_climas))
