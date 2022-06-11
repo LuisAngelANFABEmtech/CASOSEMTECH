4a3bf77ee163878e4414c037896375146530af7b
LuisAngelANFABEmtech <107011128+LuisAngelANFABEmtech@users.noreply.github.com>
2022-06-11 17:36:15 -0600

    Ejercicio #2
    Primera version ejercicio 2

---
+++ Casoretador2.py
@@ -0,0 +1,18 @@
+Municipo_1=input("ingresa un municipio: ")
+
+Habitantes_municipio1=input("ingresa número habitantes del municipio: ")
+
+Municipio_2=input("ingresa otro municipio: ")
+
+Habitantes_municipio2=input("ingresa número de habitantes del municipio anterior: ")
+
+Municipio_3=input("Ingresa el municipio final: ")
+Habitantes_municipio3=input("ingresa número habitantes del municipio anterior: ")
+
+Suma=(int(Habitantes_municipio1)+int(Habitantes_municipio2)+int(Habitantes_municipio3))
+
+print("La suma de habitantes de los municipios seleccionados es de: ",(Suma))
+
+Promediio=(Suma/3)
+
+print("el promedio es de : ", (Promediio))
